ccea3e2b86bf5fda794a170663f08599f67b1175
succatash <mstein1284@gmail.com>
2021-07-18 15:37:49 -0400

    added some styling and arrow functions

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-import React from "react"
-import Header from "./header"
-import MemeGenerator from "./memeGenerator"
-import "./App.css";
+import React from 'react';
+import Header from './header';
+import MemeGenerator from './memeGenerator';
+import './styles/App.css';
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <MemeGenerator />
-        </div>
-    )
+	return (
+		<div>
+			<Header />
+			<MemeGenerator />
+		</div>
+	);
 }
 
-export default App
+export default App;
